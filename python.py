a272f785e8eb4e3557e10faff8fd217aeb0be55e
Sonu0126 <imsonikasubedi@gmail.com>
2023-07-18 20:15:09 -0500

    added

---
+++ python.py
@@ -0,0 +1,14 @@
+print("hello")
+
+
+#git status - to track new files and to change over files
+#git init - initialize repo with git
+#git add . - all the files will move to staging
+#git add python.py - python.py file will move to staging
+#git commit -m "learningpython"(configure email address and username)
+
+# go to githun and create new repository
+#add and make it public
+#create a main branch
+#connect a local repo to git remote repo
+#puch the code and check repo
